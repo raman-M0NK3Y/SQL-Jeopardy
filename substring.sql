056c4875084fe430a4f92e6690054ca0fa4e3719
raman-M0NK3Y <45298755+raman-M0NK3Y@users.noreply.github.com>
2019-03-18 22:48:52 -0400

    Update substring.sql

--- substring.sql
+++ substring.sql
@@ -1,3 +1,12 @@
 select title, year, cat
 from yrb_book
 where lower(title) like '%'||lower(cat) || '%';
+
+
+
+TITLE                     YEAR   CAT
+------------------------- ------ ----------
+Yum, Yum, English Cooking   1993 cooking
+Humor in SQL                2000 humor
+
+  2 record(s) selected.
