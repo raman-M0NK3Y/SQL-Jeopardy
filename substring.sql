fd8283cb2b234ae71238b1e4f3b9029f710943ef
raman-M0NK3Y <45298755+raman-M0NK3Y@users.noreply.github.com>
2019-03-18 22:50:08 -0400

    Update substring.sql

--- substring.sql
+++ substring.sql
@@ -4,9 +4,4 @@ where lower(title) like '%'||lower(cat) || '%';
 
 
 
-TITLE                     YEAR   CAT
-------------------------- ------ ----------
-Yum, Yum, English Cooking   1993 cooking
-Humor in SQL                2000 humor
 
-  2 record(s) selected.
